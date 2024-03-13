000366d4b33b7321e94dbdf75e886b7b9f2fbfd3
Isabel Chong <IsabelChong.CYJI@gmail.com>
2024-03-13 13:54:32 +0800

    Add more input types into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import Welcome from "./components/Header"
 import TextField from '@mui/material/TextField'
-import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 export default function App() {
     const [type, setType] = React.useState('');
@@ -17,13 +21,16 @@ export default function App() {
     return (
         <>
         <Welcome />
+        {/* Row Name */}
         <TextField id="row-name-text" label="Row Name" variant="outlined" />
 
+        <p></p>
+        {/* Dropdown Type Selector */}
         <FormControl sx={{minWidth: 150}}>
             <InputLabel id="select-type-label">Type</InputLabel>
             <Select
-            labelId="select-row"
-            id="demo-simple-select-autowidth"
+            labelId="select-type-label"
+            id="select-input-type"
             value={type}
             onChange={handleChange}
             autoWidth
@@ -34,6 +41,36 @@ export default function App() {
             <MenuItem value={3}>Type 3</MenuItem>
             </Select>
         </FormControl>
+
+        <p></p>
+        {/* Number Input */}
+        <TextField id="min-num-text" label="Min Number" inputProps={{ type: 'number'}} />
+        <TextField id="max-num-text" label="Max Number" inputProps={{ type: 'number'}} />
+        <TextField id="num-decimals-text" label="Number of Decimals" inputProps={{ type: 'number'}} />
+
+
+        <p></p>
+        {/* String (Not processed for list yet) Input */}
+        <TextField id="string-input-text" label="List Input" variant="outlined" />
+
+        <p></p>
+        {/* Date Picker */}
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker label="Start Date"/>
+            <DatePicker label="End Date"/>
+        </LocalizationProvider>
+
+        <p></p>
+        {/* Time Picker */}
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <TimePicker label="Start Time" />
+            <TimePicker label="End Time" />
+        </LocalizationProvider>
+        
+        <p></p>
+        {/* Percentage of Blanks Input */}
+        <TextField id="percent-blank-text" label="Percentage of Blanks" InputProps={{ type: 'number',
+            endAdornment: '%'}} />
         </>
     )
 }
