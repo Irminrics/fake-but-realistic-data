c3d597801645531a53212ec63ea9f2462e16e360
Isabel Chong <IsabelChong.CYJI@gmail.com>
2024-03-14 09:41:35 +0800

    Fix column flex bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import * as React from 'react';
-import Welcome from './components/Header'
+import Header from './components/Header';
 import { basicInputTypes } from './inputType';
 
-import TextField from '@mui/material/TextField'
+import TextField from '@mui/material/TextField';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
 
 export default function App() {
-    const [type, setType] = React.useState("");
+    const [type, setType] = React.useState('');
     const [rows, setRows] = React.useState([{ name: '', type: '', blanks: '' }]);
     const [format, setFormat] = React.useState('JSON');
 
@@ -39,18 +34,18 @@ export default function App() {
         if (!selectedInputTypeInfo || !selectedInputTypeInfo.options || selectedInputTypeInfo.options.length === 0) {
             return null;
         }
-    
+
         // Render input fields based on options
         return (
-            <div style={{ display: 'flex' }}>
+            <div style={{ minWidth: 600, display: 'flex' }}>
                 {selectedInputTypeInfo.options.map(option => {
                     switch (option) {
                         case 'address-line-text':
-                            return <TextField key={option} id="address-line-text" label="Address Line" variant="outlined" />;
+                            return <TextField key={option} id="address-line-text" label="Address Line" variant="outlined" sx={{ minWidth: 400, marginRight: '10px' }} />;
                         case 'min-num-text':
-                            return <TextField key={option} id="min-num-text" label="Min Number" inputProps={{ type: 'number'}} />;
+                            return <TextField key={option} id="min-num-text" label="Min Number" inputProps={{ type: 'number' }} sx={{ marginRight: '10px' }} />;
                         case 'max-num-text':
-                            return <TextField key={option} id="max-num-text" label="Max Number" inputProps={{ type: 'number'}} />;
+                            return <TextField key={option} id="max-num-text" label="Max Number" inputProps={{ type: 'number' }} sx={{ marginRight: '10px' }} />;
                         default:
                             return null;
                     }
@@ -61,18 +56,18 @@ export default function App() {
 
     return (
         <>
-            <Welcome/>
+            <Header />
             <div style={{ display: 'flex', alignItems: 'center', fontWeight: 'bold', marginBottom: '10px' }}>
-                <div style={{ minWidth: 200, flex: 1, marginLeft: '10px' }}>Row Name</div>
-                <div style={{ minWidth: 200, flex: 1, marginLeft: '10px' }}>Input Type</div>
-                <div style={{ minWidth: 100, flex: 1, marginLeft: '10px' }}>Blanks</div>
-                <div style={{ minWidth: 200, flex: 1, marginLeft: '10px' }}>Other Options</div>
+                <div style={{ minWidth: 300, marginLeft: '10px' }}>Row Name</div>
+                <div style={{ minWidth: 200, marginLeft: '10px' }}>Input Type</div>
+                <div style={{ minWidth: 100, width: 100, marginLeft: '10px' }}>Blanks</div>
+                <div style={{ minWidth: 300, marginLeft: '10px' }}>Other Options</div>
             </div>
             {rows.map((row, index) => (
                 <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
                     {/* Row Name */}
                     <TextField
-                        sx={{minWidth: 200, flex: 1, marginLeft: '10px' }}
+                        sx={{ minWidth: 300, marginLeft: '10px' }}
                         value={row.name}
                         onChange={(e) => {
                             const newName = e.target.value;
@@ -80,11 +75,10 @@ export default function App() {
                         }}
                         label=""
                         variant="outlined"
-                        style={{ flex: 1 }}
                     />
 
                     {/* Dropdown Type Selector */}
-                    <FormControl sx={{minWidth: 200, flex: 1, marginLeft: '10px' }}>
+                    <FormControl sx={{ minWidth: 200, marginLeft: '10px' }}>
                         <InputLabel id={`select-type-label-${index}`}></InputLabel>
                         <Select
                             labelId={`select-type-label-${index}`}
@@ -101,10 +95,10 @@ export default function App() {
                             ))}
                         </Select>
                     </FormControl>
-                    
+
                     {/* Percentage of Blanks Input */}
                     <TextField
-                        sx={{minWidth: 100, flex: 1, marginLeft: '10px' }}
+                        sx={{ minWidth: 100, width: 100, marginLeft: '10px' }}
                         value={row.blanks}
                         onChange={(e) => {
                             const newBlanks = e.target.value;
@@ -116,22 +110,22 @@ export default function App() {
                             endAdornment: '%',
                             inputProps: { min: 0, max: 100 }
                         }}
-                        style={{ flex: 1 }}
                         placeholder="0"
                     />
 
                     {/* Other Options */}
-                    <div style={{ minWidth: 200, flex: 1, marginLeft: '10px' }}>
+                    <div style={{ minWidth: 300, marginLeft: '10px'}}>
                         {renderInputFields(basicInputTypes.find(inputType => inputType.name === row.type))}
                     </div>
                 </div>
             ))}
 
+            {/* Add New Row Button */}
             <AddCircleIcon onClick={handleAddRow} style={{ marginLeft: 'auto', cursor: 'pointer' }} />
 
+            <Divider style={{ margin: '20px 0' }} />
 
             {/* Generate Data Button with Format Dropdown Picker */}
-            <Divider style={{ margin: '20px 0' }} />
             <div style={{ display: 'flex', alignItems: 'center' }}>
                 <Button variant="contained" color="primary" style={{ marginRight: '10px' }}>
                     Generate Data
