ed14e9d0917d47a54f9c22da401bfd6e8217f79e
tantzeyeong <tzeyeongtan@gmail.com>
2024-03-18 22:12:48 +0800

    Add generation functionality for remaining data types

---
+++ src/timeFormatListTypes.jsx
@@ -0,0 +1,26 @@
+export const timeFormatListTypes = [
+    {
+        name: '12 Hour',
+        value: "h:mm A" // or "hh:mm A" if you want a leading zero for hours
+    },
+    {
+        name: '12 Hour w/seconds',
+        value: "h:mm:ss A" // or "hh:mm:ss A" for leading zero
+    },
+    {
+        name: '12 Hour w/millis',
+        value: "h:mm:ss.SSS A" // or "hh:mm:ss.SSS A" for leading zero
+    },
+    {
+        name: '24 Hour',
+        value: "HH:mm"
+    },
+    {
+        name: '24 Hour w/seconds',
+        value: "HH:mm:ss"
+    },
+    {
+        name: '24 Hour w/millis',
+        value: "HH:mm:ss.SSS"
+    },
+];
