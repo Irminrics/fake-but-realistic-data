626a2553ebd83bcaee5f8a5f4fe55ded1cac62d4
Jun Kang <irminrics@gmail.com>
2024-03-27 11:01:55 +0800

    Remove .vite directory

--- .vite/deps/dayjs_plugin_customParseFormat.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_customParseFormat
-} from "./chunk-ONUA4HX3.js";
-import "./chunk-CEQRFMJQ.js";
-export default require_customParseFormat();
-//# sourceMappingURL=dayjs_plugin_customParseFormat.js.map
